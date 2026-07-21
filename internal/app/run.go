2c0a79d667b8a8c217527b4f0d1f1d8ed757c389
tanahiro2010 <herentongkegu087@gmail.com>
2026-07-21 15:14:41 +0900

    feat: それぞれのクライアントの初期化処理を追加

--- internal/app/run.go
+++ internal/app/run.go
@@ -1,11 +1,29 @@
 package app
 
+import (
+	"analyze_narou/internal/client/gpt"
+
+	"github.com/sashabaranov/go-openai"
+)
+
 type Config struct {
-	NarouUrl     string
-	OpenAIApiKey string
+	NarouUrl          string
+	OpenAIApiKey      string
+	DiscordWebhookURL string
 }
 
 func Run(config Config) error {
+	openaiClient := gpt.NewOpenAIClient(gpt.OpenAIConfig{
+		Model:  openai.GPT3Dot5Turbo,
+		ApiKey: config.OpenAIApiKey,
+	})
 
+	narouClient := narou.NewNarouClient(narou.NarouConfig{
+		Endpoint: config.NarouUrl,
+	})
+
+	discordClient := discord.NewDiscordClient(discord.DiscordConfig{
+		WebhookURL: config.DiscordWebhookURL,
+	})
 	return nil
 }
