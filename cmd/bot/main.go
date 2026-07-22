860f8d59f90defb98bc2f72bd3296cd0411ff232
tanahiro2010 <herentongkegu087@gmail.com>
2026-07-23 00:14:56 +0900

    feat; 環境変数読み込みを追加

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"analyze_narou/internal/app"
 	"analyze_narou/internal/client/narou"
+	"analyze_narou/internal/utils"
 	"os"
 )
 
 func main() {
+	utils.LoadDotEnv(".env")
+
 	config := app.Config{
 		NarouUrl:          os.Getenv("NAROU_URL"),
 		OpenAIApiKey:      os.Getenv("OPENAI_API_KEY"),
 		DiscordWebhookURL: os.Getenv("DISCORD_WEBHOOK_URL"),
 	}
 
-	mode := narou.RankingModeDaily // You can change this to the desired ranking mode
+	mode := narou.RankingModeDaily // cmdのargから指定できるように
 
 	app.Run(config, mode)
 }
