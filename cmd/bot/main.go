b65bf0a6920b80479bfef2b078a869c6c1f8b888
tanahiro2010 <herentongkegu087@gmail.com>
2026-07-22 17:11:50 +0900

    chore: 環境変数読み込みに修正

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "analyze_narou/internal/app"
+import (
+	"analyze_narou/internal/app"
+	"os"
+)
 
 func main() {
+
 	config := app.Config{
-		"https://api.syosetu.com/",
-		"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",
-		"https://discord.com/api/webhooks/1416959680076447764/2fBoIjV2dXhLxyX1IbLGROkb9excq8FRTT8wwov9Vv1Ui18vIHzfDnorR1qH25mGcI9p",
+		os.Getenv("NAROU_URL"),
+		os.Getenv("OPENAI_API_KEY"),
+		os.Getenv("DISCORD_WEBHOOK_URL"),
 	}
 
 	app.Run(config)
