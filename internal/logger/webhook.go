cc6aead218e9e696d3d7cb530e1275588039ba7e
tanahiro2010 <herentongkegu087@gmail.com>
2026-07-22 23:52:22 +0900

    feat: ロガーの雛形を追加

--- internal/logger/webhook.go
+++ internal/logger/webhook.go
@@ -1,1 +1,33 @@
 package logger
+
+import (
+	"analyze_narou/internal/analytics"
+	"analyze_narou/internal/client/discord"
+)
+
+type WebhookLogger struct {
+	DiscordClient discord.DiscordClient
+}
+
+func NewWebhookLogger(discordClient discord.DiscordClient) *WebhookLogger {
+	return &WebhookLogger{
+		DiscordClient: discordClient,
+	}
+}
+
+func (w *WebhookLogger) Log(message string) error {
+
+	return nil
+}
+
+func (w *WebhookLogger) GenreAnalyzeResult(ctx analytics.GenreAnalyzeResult) error {
+	message := ""
+
+	return w.Log(message)
+}
+
+func (w *WebhookLogger) AllAnalyzeResult(ctx analytics.AllAnalyzeResult) error {
+	message := ""
+
+	return w.Log(message)
+}
